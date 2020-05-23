786aba0196ec7932fffd3d22edb3a598935776a3
azrehman <abdullahzr@vt.edu>
2020-05-22 23:47:50 -0400

    changed np.dot in loop to np.matmul

--- layer.py
+++ layer.py
@@ -2,6 +2,8 @@ import numpy as np
 
 # inputs for this neuron which are outputs of neurons from previous layer
 inputs = np.array([1, 2, 3, 2.5])
+
+# each row in weights array is the weights for one neuron in layer
 weights = np.array([[0.2, 0.8, -0.5, 1.0],
                     [0.5, -0.91, 0.26, -0.5],
                     [-0.26, -0.27, 0.17, 0.87]])
@@ -9,8 +11,6 @@ weights = np.array([[0.2, 0.8, -0.5, 1.0],
 biases = np.array([2, 3, 0.5])
 
 layer_outputs = []
-for neuron_weights, neuron_bias in zip(weights, biases):
-    neuron_output = 0
-    neuron_output = np.dot(inputs, neuron_weights) + neuron_bias
-    layer_outputs.append(neuron_output)
+#                                broadcasting add
+layer_outputs = np.matmul(weights, inputs) + biases
 print(layer_outputs)
