6cf87a5955675980d26310bf3f15c15c4f1f5d31
azrehman <abdullahzr@vt.edu>
2020-05-23 00:44:50 -0400

    changed input to batch of inputs

--- layer.py
+++ layer.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 # inputs for this neuron which are outputs of neurons from previous layer
-inputs = np.array([1, 2, 3, 2.5])
+inputs = np.array([[1, 2, 3, 2.5],
+                   [2.0, 5.0, -1.0, 2.0],
+                   [-1.5, 2.7, 3.3, -0.8]])
 
 # each row in weights array is the weights for one neuron in layer
 weights = np.array([[0.2, 0.8, -0.5, 1.0],
@@ -10,7 +12,10 @@ weights = np.array([[0.2, 0.8, -0.5, 1.0],
 
 biases = np.array([2, 3, 0.5])
 
+# layer_outputs = np.zeros(inputs.shape[1])
 layer_outputs = []
-#                                broadcasting add
-layer_outputs = np.matmul(weights, inputs) + biases
+
+
+#                                 broadcasting add
+layer_outputs = np.matmul(inputs, weights.T) + biases
 print(layer_outputs)
