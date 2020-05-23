6410a3a00312849bd953ae388980d55d0818336d
azrehman <abdullahzr@vt.edu>
2020-05-23 18:54:54 -0400

    added relu activation implementation

--- layer.py
+++ layer.py
@@ -1,21 +1,29 @@
 import numpy as np
 
-# inputs for this neuron which are outputs of neurons from previous layer
-inputs = np.array([[1, 2, 3, 2.5],
-                   [2.0, 5.0, -1.0, 2.0],
-                   [-1.5, 2.7, 3.3, -0.8]])
-
-# each row in weights array is the weights for one neuron in layer
-weights = np.array([[0.2, 0.8, -0.5, 1.0],
-                    [0.5, -0.91, 0.26, -0.5],
-                    [-0.26, -0.27, 0.17, 0.87]])
-
-biases = np.array([2, 3, 0.5])
-
-# layer_outputs = np.zeros(inputs.shape[1])
-layer_outputs = []
+np.random.seed(0)
+# batches of input, each row is inputs for one batch
+X = np.array([[1, 2, 3, 2.5],
+              [2.0, 5.0, -1.0, 2.0],
+              [-1.5, 2.7, 3.3, -0.8]])
 
 
-#                                 broadcasting add
-layer_outputs = np.matmul(inputs, weights.T) + biases
-print(layer_outputs)
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.1 * np.random.standard_normal((n_inputs, n_neurons))
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.matmul(inputs, self.weights) + self.biases
+
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
+
+layer1.forward(X)
+layer2.forward(layer1.output)
+print(layer2.output)
